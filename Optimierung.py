76da809a6ec75fa184f04a81e591b94660ae5ac3
123occasional <148688553+123occasional@users.noreply.github.com>
2025-03-04 07:10:11 +0100

    Update Optimierung.py

--- Optimierung.py
+++ Optimierung.py
@@ -1,121 +1,92 @@
 import Game
-import PlayerA
+import PlayerA1
 import random
 
-class Optimierung:
-    def __init__(self):
-        pass
 
-    def eins_gegen_eins(self, spieler_0, spieler_1):
-        game1 = Game.Game()
-        game1.spiel(spieler_0,spieler_1)
-        return game1.moves
-
-    def runde(self, liste):
-        spieler_x = PlayerA.PlayerA(liste[0], liste[1], liste[2], 2)
-        spieler_y = PlayerA.PlayerA(liste[3], liste[4], liste[5], 2)
-        spieler_z = PlayerA.PlayerA(liste[6], liste[7], liste[8], 2)
-        score_x = [0,0]
-        score_y = [0,0]
-        score_z = [0,0]
-        i = 0
-        while i < 100:
-            spieler_rand = PlayerA.PlayerA(random.random(), random.random(), random.random(), 2)
-            vergleich_xrand = self.vergleich(spieler_x, spieler_rand)
-            vergleich_yrand = self.vergleich(spieler_y, spieler_rand)
-            vergleich_zrand = self.vergleich(spieler_z, spieler_rand)
-            score_x = [score_x[0] + vergleich_xrand[0], score_x[1] + vergleich_xrand[1]]
-            score_y = [score_y[0] + vergleich_yrand[0], score_y[1] + vergleich_yrand[1]]
-            score_z = [score_z[0] + vergleich_zrand[0], score_z[1] + vergleich_zrand[1]]
-            i += 1
-        print(score_x)
-        print(score_y)
-        print(score_z)
-        if score_x[0] > score_y[0]:
-            if score_x[0] > score_z[0]:
-                win = "x"
-            elif score_x[0] < score_z[0]:
-                win = "z"
-            elif score_x[1] < score_z[1]:
-                win = "x"
-            elif score_z[1] < score_x[1]:
-                win = "z"
-            else:
-                win = liste[9]
-        elif score_y[0] > score_x[0]:
-            if score_y[0] > score_z[0]:
-                win = "y"
-            elif score_y[0] < score_z[0]:
-                win = "z"
-            elif score_y[1] < score_z[1]:
-                win = "y"
-            elif score_z[1] < score_y[1]:
-                win = "z"
-            else:
-                win = liste[9]
-        elif score_z[0] > score_x[0]:
-            win = "z"
-        elif score_x[0] > score_z[0]:
-            if score_x[1] < score_y[1]:
-                win = "x"
-            elif score_y[1] < score_x[1]:
-                win = "y"
-            else:
-                win = liste[9]
-        else:
-            if score_x[1] < score_y[1]:
-                if score_x[1] < score_z[1]:
-                    win = "x"
-                elif score_z[1] < score_x[1]:
-                    win = "z"
-                else:
-                    win = liste[9]
-            else:
-                if score_y[1] < score_z[1]:
-                    win = "y"
-                elif score_z[1] < score_y[1]:
-                    win = "z"
-                else:
-                    win = liste[9]
-        print(win)
-        return win
-
-
-    def vergleich(self, spieler_0, spieler_rand):
-        game_01 = self.eins_gegen_eins(spieler_0, spieler_rand)
-        game_10 = self.eins_gegen_eins(spieler_rand, spieler_0)
-        if game_01 % 2 ^ 1 < 1:
-            if game_10 % 2 ^ 1 > 0:
-                score_0 = [2, (game_01 + game_10)/2]
-            else:
-                score_0 = [1, game_01]
-        else:
-            if game_10 % 2 ^ 1 < 1:
-                score_0 = [0, 0]
-            else:
-                score_0 = [1, game_10]
-        return score_0
-
-    def angleichen(self, win, liste, factor):
-        i = 0
-        if win == "x":
-            while i < 6:
-                liste[i+3] = liste[i+3] + (liste[i%3] - liste[i+3]) * factor
-                i += 1
-        elif win == "y":
-            while i < 6:
-                liste[(i+6)%9] = liste[(i+6)%9] + (liste[i%3+3] - liste[(i+6)%9]) * factor
-                i += 1
-        else:
-            while i < 6:
-                liste[i] = liste[i] + (liste[i%3+6]-liste[i]) * factor
-                i += 1
-        return liste
-
-    def optimieren(self, liste, factor, depth):
-        if depth == 0:
-            return liste
-        liste[9] = self.runde(liste)
-        liste = self.angleichen(liste[9], liste, factor)
+def optimieren(liste, number_games, factor, depth):
+    for i in range(depth):
         print(liste)
-        return self.optimieren(liste, factor, depth-1)
+        print("runde " + str(i))
+        liste[9] = runde(liste, number_games)
+        liste = angleichen(liste[9], liste, factor)
+    return liste
+
+
+def eins_gegen_eins(spieler_0, spieler_1):
+    game1 = Game.Game((spieler_0, spieler_1))
+    game1.play()
+    return [game1.state.winner, game1.moves]
+
+
+def runde(liste, number_games):
+    spieler_x = PlayerA1.PlayerA1(liste[0], liste[1], liste[2], 2)
+    spieler_y = PlayerA1.PlayerA1(liste[3], liste[4], liste[5], 2)
+    spieler_z = PlayerA1.PlayerA1(liste[6], liste[7], liste[8], 2)
+
+    score_x = score_y = score_z = [0, 0]
+    for i in range(number_games):
+        print(str(i) + "/" + str(number_games))
+        spieler_rand = PlayerA1.PlayerA1(random.random(), random.random(), random.random(), 2)
+        score_x = liste_addieren(score_x, vergleich(spieler_x, spieler_rand))
+        score_y = liste_addieren(score_y, vergleich(spieler_y, spieler_rand))
+        score_z = liste_addieren(score_z, vergleich(spieler_z, spieler_rand))
+    print(score_x)
+    print(score_y)
+    print(score_z)
+    siege = {score_x[0], score_y[0], score_z[0]}
+    meiste_siege = max(siege)
+    if len(siege) == 3:  # überprüfen
+        if meiste_siege == score_x[0]:
+            win = "x"
+        elif meiste_siege == score_y[0]:
+            win = "y"
+        elif meiste_siege == score_z[0]:
+            win = "z"
+    else:
+        züge = [x[1] for x in (score_x, score_y, score_z) if x[0] == meiste_siege]
+        if len(set(züge)) == len(züge):
+            wenigste_züge = min(züge)
+            if wenigste_züge == score_x[1]:
+                win = "x"
+            elif wenigste_züge == score_y[1]:
+                win = "y"
+            elif wenigste_züge == score_z[1]:
+                win = "z"
+        else:
+            win = liste[9]
+    print(win)
+    return win
+
+
+def vergleich(spieler_0, spieler_rand):
+    game_01 = eins_gegen_eins(spieler_0, spieler_rand)
+    game_10 = eins_gegen_eins(spieler_rand, spieler_0)
+    if game_01[0] == 0:
+        if game_10[0] == 1:
+            score_0 = [2, (game_01[1] + game_10[1])]
+        else:
+            score_0 = [1, game_01[1]]
+    else:
+        if game_10[0] == 0:
+            score_0 = [0, 0]
+        else:
+            score_0 = [1, game_10[1]]
+    return score_0
+
+
+def angleichen(win, liste, factor):
+    match win:
+        case "x":
+            for i in range(6):
+                liste[i + 3] = liste[i + 3] + (liste[i % 3] - liste[i + 3]) * factor
+        case "y":
+            for i in range(6):
+                liste[(i + 6) % 9] = liste[(i + 6) % 9] + (liste[i % 3 + 3] - liste[(i + 6) % 9]) * factor
+        case "z":
+            for i in range(6):
+                liste[i] = liste[i] + (liste[i % 3 + 6] - liste[i]) * factor
+    return liste
+
+
+def liste_addieren(liste1, liste2):
+    return [a + b for a, b in zip(liste1, liste2)]
--- Test.py
+++ Test.py
@@ -5,12 +5,11 @@ import PlayerB
 import GameState
 import Optimierung
 
-
+'''
 player_a_1 = PlayerA1.PlayerA1(3, 7, 0, 4)
 player_a_2 = PlayerA1.PlayerA1(3, 7, 0, 4)
 
 game1 = Game.Game((player_a_1, player_a_2))
-game1.play()
+game1.play()'''
 
-opti = Optimierung.Optimierung()
-print(opti.optimieren([1,0,0,0,1,0,0,0,1,"x"], 0.2, 50))
+print(Optimierung.optimieren([1, 0, 0, 0, 1, 0, 0, 0, 1, "x"], 10, 0.2, 10))
