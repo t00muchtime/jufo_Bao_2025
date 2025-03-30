5520fc3dc0fc9304dbefdafdbf42a327f39c6d24
t00muchtime <143702352+t00muchtime@users.noreply.github.com>
2025-03-30 23:12:47 +0200

    game board with numbers

--- GUITest.py
+++ GUITest.py
@@ -1,10 +1,11 @@
 import pygame as pg
 
 import GameState
+import PlayerA1
 
 pg.init()
 
-# font = pg.font.Font(None, 40)
+font = pg.font.Font(None, 40)
 
 SIZE = WIDTH, HEIGHT = 500, 300
 SCREEN_START, SCREEN_GAME, SCREEN_GAMEOVER = 0, 1, 2
@@ -19,7 +20,9 @@ screen = pg.display.set_mode(SIZE, pg.RESIZABLE)
 clock = pg.time.Clock()
 
 # board
-board = [[2] * 16] * 2
+human = 0
+computer = PlayerA1.PlayerA1(0.69, 0.31, 0, 3)
+board = [[2] * 16, [2] * 16]
 game_state = GameState.GameState(board, 0)
 current_player = 0
 move = False
@@ -36,11 +39,12 @@ rects = [
 ]
 rects = [rects[1][8:] + rects[1][7::-1], rects[0][7::-1] + rects[0][8:]]
 
-start_button = pg.Rect(10, 20, 30, 30)
+first_button = pg.Rect(10, 20, 60, 30)
+second_button = pg.Rect(10, 60, 60, 30)
 win_button = pg.Rect(30, 30, 30, 30)
-menu_button = pg.Rect(10, 20, 30, 30)
-restart_button = pg.Rect(50, 20, 30, 30)
-quit_button = pg.Rect(90, 20, 30, 30)
+menu_button = pg.Rect(10, 220, 30, 30)
+restart_button = pg.Rect(50, 220, 30, 30)
+quit_button = pg.Rect(90, 220, 30, 30)
 
 # game loop
 while True:
@@ -50,12 +54,18 @@ while True:
                 pg.quit()
                 raise SystemExit
             elif event.type == pg.MOUSEBUTTONDOWN:
-                if start_button.collidepoint(pg.mouse.get_pos()):
-                    print("start button pressed")
+                if first_button.collidepoint(pg.mouse.get_pos()):
+                    human = 0
+                    print("first button pressed")
+                    screen_id = SCREEN_GAME
+                elif second_button.collidepoint(pg.mouse.get_pos()):
+                    human = 1
+                    print("second button pressed")
                     screen_id = SCREEN_GAME
 
         screen.fill("black")
-        pg.draw.rect(screen, "white", start_button)
+        pg.draw.rect(screen, "white", first_button)
+        pg.draw.rect(screen, "white", second_button)
         pg.display.flip()
         clock.tick(60)
 
@@ -66,23 +76,32 @@ while True:
                 pg.quit()
                 raise SystemExit
             elif event.type == pg.MOUSEBUTTONDOWN and not move:
-                for i in game_state.moves:
-                    if rects[current_player][i].collidepoint(pg.mouse.get_pos()):
-                        pit = i
-                        print("pit: " + str(pit))
+                if current_player == human:
+                    for i in game_state.moves:
+                        if rects[current_player][i].collidepoint(pg.mouse.get_pos()):
+                            pit = i
+                            print("pit: " + str(pit))
+                            try:
+                                game_state = game_state.generate_child(pit)
+                            except:
+                                break
+                            move = True
+                            pg.time.set_timer(pg.event.Event(3), 500)
+                            hand += board[current_player][pit]
+                            board[current_player][pit] = 0
+                            pit = (pit + 1) % 16
+                else:
+                    pit = computer.best_move(game_state)
+                    print("pit: " + str(pit))
+                    game_state = game_state.generate_child(pit)
+                    move = True
+                    pg.time.set_timer(pg.event.Event(3), 500)
+                    hand += board[current_player][pit]
+                    board[current_player][pit] = 0
+                    pit = (pit + 1) % 16
 
-                        move = True
-                        pg.time.set_timer(pg.event.Event(3), 100)
-                        temp = game_state.generate_child(pit)
-                        hand += board[current_player][pit]
-                        board[current_player][pit] = 0
-                        print(temp.board)
-                        print("hello")
-                        pit = (pit + 1) % 16
-                        # else:
-
-            elif move and event.type == 3:
-                print("move")
+            elif move and event.type == pg.MOUSEBUTTONDOWN:
+                print("move: ")
                 print(board)
                 if hand > 1:
                     board[current_player][pit] += 1
@@ -102,23 +121,17 @@ while True:
                     pg.time.set_timer(pg.event.Event(3), 0)
                     move = False
                     current_player ^= 1
-                    game_state = GameState.GameState(board, current_player)
                     if game_state.winner is not None:
-                        board = [[2] * 16] * 2
-                        game_state = GameState.GameState(board, 0)
                         screen_id = SCREEN_GAMEOVER
                     print(game_state.board)
-                    print(temp.board)
-                    print(game_state.board == temp.board)
                     print(current_player)
 
         screen.fill("black")
         for i in range(2):
             for j in range(16):
-                if i == current_player and j in game_state.moves:
-                    pg.draw.rect(screen, "green", rects[i][j])
-                else:
-                    pg.draw.rect(screen, "red", rects[i][j])
+                screen.blit(font.render(str(board[i][j]), True, "white"),
+                            rects[i][j].topleft)
+        screen.blit(font.render(str(hand), True, "blue"), (350, 30))
         pg.display.flip()
         clock.tick(60)
 
@@ -130,17 +143,27 @@ while True:
                 raise SystemExit
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if menu_button.collidepoint(pg.mouse.get_pos()):
+                    board = [[2] * 16, [2] * 16]
+                    game_state = GameState.GameState(board, 0)
                     print("menu button pressed: returning to start")
                     screen_id = SCREEN_START
                 elif restart_button.collidepoint(pg.mouse.get_pos()):
+                    board = [[2] * 16, [2] * 16]
+                    game_state = GameState.GameState(board, 0)
                     print("restart button pressed: new game")
                     screen_id = SCREEN_GAME
                 elif quit_button.collidepoint(pg.mouse.get_pos()):
+                    board = [[2] * 16, [2] * 16]
+                    game_state = GameState.GameState(board, 0)
                     print("quit button pressed: quit game")
                     pg.quit()
                     raise SystemExit
 
         screen.fill("black")
+        for i in range(2):
+            for j in range(16):
+                screen.blit(font.render(str(board[i][j]), True, "white"),
+                            rects[i][j].topleft)
         pg.draw.rect(screen, "green", menu_button)
         pg.draw.rect(screen, "yellow", restart_button)
         pg.draw.rect(screen, "red", quit_button)
